b9252073c41717b41ff53ee83499a3f27ddd9833
Sander Ruscigno <ruscigno@gmail.com>
2022-12-30 14:33:05 -0600

    feat: go server, first commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/Ruscigno/docker-mt5
+
+go 1.19
+
+require (
+	github.com/blendle/zapdriver v1.3.1
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.2.0
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"io"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/Ruscigno/docker-mt5/src/settings"
+	"github.com/blendle/zapdriver"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type WriteSyncer struct {
+	io.Writer
+}
+
+func (ws WriteSyncer) Sync() error {
+	return nil
+}
+
+func connectsToPostgres(config *settings.Settings) (*sqlx.DB, error) {
+	zap.L().Info("Trying to connect to the database")
+	db, err := sqlx.Connect("postgres", config.DatabaseDSN)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(time.Minute)
+	return db, nil
+}
+
+func SetupLogger(fileName string) *zap.Logger {
+	// The bundled Config struct only supports the most common configuration
+	// options. More complex needs, like splitting logs between multiple files
+	// or writing to non-file outputs, require use of the zapcore package.
+	//
+	// In this example, imagine we're both sending our logs to Kafka and writing
+	// them to the console. We'd like to encode the console output and the Kafka
+	// topics differently, and we'd also like special treatment for
+	// high-priority logs.
+
+	// First, define our level-handling logic.
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl < zapcore.ErrorLevel
+	})
+
+	// Assume that we have clients for two Kafka topics. The clients implement
+	// zapcore.WriteSyncer and are safe for concurrent use. (If they only
+	// implement io.Writer, we can use zapcore.AddSync to add a no-op Sync
+	// method. If they're not safe for concurrent use, we can add a protecting
+	// mutex with zapcore.Lock.)
+	logFile := GetWriteSyncer(fileName)
+	topicDebugging := zapcore.AddSync(logFile)
+	topicErrors := zapcore.AddSync(logFile)
+
+	// High-priority output should also go to standard error, and low-priority
+	// output should also go to standard out.
+	consoleDebugging := zapcore.Lock(os.Stdout)
+	consoleErrors := zapcore.Lock(os.Stderr)
+
+	var config zap.Config
+	if "os.Getenv(utils.ConfigDebugLevel)" == "PROD" {
+		config = zap.NewProductionConfig()
+		config.EncoderConfig = zap.NewProductionEncoderConfig()
+		config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	} else {
+		config = zap.NewDevelopmentConfig()
+		config.EncoderConfig = zap.NewDevelopmentEncoderConfig()
+	}
+	config.EncoderConfig.CallerKey = zapdriver.SourceLocation(runtime.Caller(0)).String
+	configConsole := config
+	configConsole.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+
+	// Optimize the Kafka output for machine consumption and the console output
+	// for human operators.
+	kafkaEncoder := zapcore.NewJSONEncoder(config.EncoderConfig)
+	consoleEncoder := zapcore.NewConsoleEncoder(configConsole.EncoderConfig)
+
+	// Join the outputs, encoders, and level-handling functions into
+	// zapcore.Cores, then tee the four cores together.
+	core := zapcore.NewTee(
+		zapcore.NewCore(kafkaEncoder, topicErrors, highPriority),
+		zapcore.NewCore(consoleEncoder, consoleErrors, highPriority),
+		zapcore.NewCore(kafkaEncoder, topicDebugging, lowPriority),
+		zapcore.NewCore(consoleEncoder, consoleDebugging, lowPriority),
+	)
+
+	// From a zapcore.Core, it's easy to construct a Logger.
+	logger := zap.New(core)
+	return logger
+}
+
+func GetWriteSyncer(logName string) zapcore.WriteSyncer {
+	var ioWriter = &lumberjack.Logger{
+		Filename:   logName,
+		MaxSize:    20, // MB
+		MaxBackups: 5,  // number of backups
+		MaxAge:     28, //days
+		LocalTime:  true,
+		Compress:   false, // disabled by default
+	}
+	var sw = WriteSyncer{
+		ioWriter,
+	}
+	return sw
+}
+
+func main() {
+	logger := SetupLogger("mt5-report-optimization-result-loader.log")
+	defer logger.Sync()
+	undo := zap.ReplaceGlobals(logger)
+	defer undo()
+	zap.L().Info("Starting service")
+	config, err := settings.LoadSettings("settings.json")
+	if err != nil {
+		zap.L().Fatal(err.Error())
+	}
+	zap.L().Info("Settings loaded", zap.Any("settings", config))
+}
