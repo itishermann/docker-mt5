222d5ca8c3d5bc12851c911d4e90776af1b218a5
Sander Ruscigno <ruscigno@gmail.com>
2023-05-14 07:21:34 -0600

    feat: tools

---
+++ build/start-new-containers.sh
@@ -0,0 +1,31 @@
+cd ./mt5-015-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-016-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-017-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-018-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-019-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-020-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-021-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-022-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-023-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ../mt5-024-monitor
+sleep 60
+docker-compose down && docker-compose up -d
+cd ..
