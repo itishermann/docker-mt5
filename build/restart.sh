fcdb5541fe90ebd71e441ba32abff8bbd4a3c4c5
Sander Ruscigno <ruscigno@gmail.com>
2023-05-27 09:36:10 -0600

    feat: restart script

---
+++ build/restart.sh
@@ -0,0 +1,47 @@
+docker restart mt5-001
+sleep 30
+docker restart mt5-002
+sleep 30
+docker restart mt5-003
+sleep 30
+docker restart mt5-004
+sleep 30
+docker restart mt5-005
+sleep 30
+docker restart mt5-006
+sleep 30
+docker restart mt5-007
+sleep 30
+docker restart mt5-008
+sleep 30
+docker restart mt5-009
+sleep 30
+docker restart mt5-010
+sleep 30
+docker restart mt5-011
+sleep 30
+docker restart mt5-012
+sleep 30
+docker restart mt5-013
+sleep 30
+docker restart mt5-014
+sleep 30
+docker restart mt5-015
+sleep 30
+docker restart mt5-016
+sleep 30
+docker restart mt5-017
+sleep 30
+docker restart mt5-018
+sleep 30
+docker restart mt5-019
+sleep 30
+docker restart mt5-020
+sleep 30
+docker restart mt5-021
+sleep 30
+docker restart mt5-022
+sleep 30
+docker restart mt5-023
+sleep 30
+docker restart mt5-024
