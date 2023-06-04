38cbaa06219a075276e40f64cfdd0dde8ff34345
Sander Ruscigno <ruscigno@gmail.com>
2023-06-04 12:27:10 -0600

    feat: update scripts

--- build/restart.sh
+++ build/restart.sh
@@ -1,47 +1,115 @@
-docker restart mt5-001
+# docker restart mt5-001
+# sleep 30
+# docker restart mt5-002
+# sleep 30
+# docker restart mt5-003
+# sleep 30
+# docker restart mt5-004
+# sleep 30
+# docker restart mt5-005
+# sleep 30
+# docker restart mt5-006
+# sleep 30
+# docker restart mt5-007
+# sleep 30
+# docker restart mt5-008
+# sleep 30
+# docker restart mt5-009
+# sleep 30
+# docker restart mt5-010
+# sleep 30
+# docker restart mt5-011
+# sleep 30
+# docker restart mt5-012
+# sleep 30
+# docker restart mt5-013
+# sleep 30
+# docker restart mt5-014
+# sleep 30
+# docker restart mt5-015
+# sleep 30
+# docker restart mt5-016
+# sleep 30
+# docker restart mt5-017
+# sleep 30
+# docker restart mt5-018
+# sleep 30
+# docker restart mt5-019
+# sleep 30
+# docker restart mt5-020
+# sleep 30
+# docker restart mt5-021
+# sleep 30
+# docker restart mt5-022
+# sleep 30
+# docker restart mt5-023
+# sleep 30
+# docker restart mt5-024
+# sleep 30
+# docker restart mt5-025
+# sleep 30
+# docker restart mt5-026
+# sleep 30
+# docker restart mt5-027
+# sleep 30
+# docker restart mt5-028
+# sleep 30
+# docker restart mt5-029
 sleep 30
-docker restart mt5-002
+docker restart mt5-030
 sleep 30
-docker restart mt5-003
+docker restart mt5-031
 sleep 30
-docker restart mt5-004
+docker restart mt5-032
 sleep 30
-docker restart mt5-005
+docker restart mt5-033
 sleep 30
-docker restart mt5-006
+docker restart mt5-034
 sleep 30
-docker restart mt5-007
+docker restart mt5-035
 sleep 30
-docker restart mt5-008
+docker restart mt5-036
 sleep 30
-docker restart mt5-009
+docker restart mt5-037
 sleep 30
-docker restart mt5-010
+docker restart mt5-038
 sleep 30
-docker restart mt5-011
+docker restart mt5-039
 sleep 30
-docker restart mt5-012
+docker restart mt5-040
 sleep 30
-docker restart mt5-013
+docker restart mt5-041
 sleep 30
-docker restart mt5-014
+docker restart mt5-042
 sleep 30
-docker restart mt5-015
+docker restart mt5-043
 sleep 30
-docker restart mt5-016
+docker restart mt5-044
 sleep 30
-docker restart mt5-017
+docker restart mt5-045
 sleep 30
-docker restart mt5-018
+docker restart mt5-046
 sleep 30
-docker restart mt5-019
+docker restart mt5-047
 sleep 30
-docker restart mt5-020
+docker restart mt5-048
 sleep 30
-docker restart mt5-021
+docker restart mt5-049
 sleep 30
-docker restart mt5-022
+docker restart mt5-050
 sleep 30
-docker restart mt5-023
+docker restart mt5-051
 sleep 30
-docker restart mt5-024
+docker restart mt5-052
+sleep 30
+docker restart mt5-053
+sleep 30
+docker restart mt5-054
+sleep 30
+docker restart mt5-055
+sleep 30
+docker restart mt5-056
+sleep 30
+docker restart mt5-057
+sleep 30
+docker restart mt5-058
--- build/start-containers.sh
+++ build/start-containers.sh
@@ -2,54 +2,67 @@ cd ./mt5-001
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-001
+
 cd ../mt5-002
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-002
+
 cd ../mt5-003
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-003
+
 cd ../mt5-004
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-004
+
 cd ../mt5-005
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-005
+
 cd ../mt5-006
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-006
+
 cd ../mt5-007
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-007
+
 cd ../mt5-008
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-008
+
 cd ../mt5-009
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-009
+
 cd ../mt5-010
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-010
+
 cd ../mt5-011
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-011
+
 cd ../mt5-012
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-012
+
 cd ../mt5-013
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-013
+
 cd ../mt5-014
 docker compose down && docker compose up -d
 sleep 30
@@ -59,75 +72,218 @@ cd ./mt5-015
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-015
+
 cd ../mt5-016
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-016
+
 cd ../mt5-017
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-017
+
 cd ../mt5-018
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-018
+
 cd ../mt5-019
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-019
+
 cd ../mt5-020
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-020
+
 cd ../mt5-021
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-021
+
 cd ../mt5-022
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-022
+
 cd ../mt5-023
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-023
+
 cd ../mt5-024
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-024
+
 cd ../mt5-025
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-025
+
 cd ../mt5-026
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-026
+
 cd ../mt5-027
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-027
+
 cd ../mt5-028
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-028
+
 cd ../mt5-029
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-029
+
 cd ../mt5-030
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-030
+
 cd ../mt5-031
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-031
+
 cd ../mt5-032
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-032
+
 cd ../mt5-033
 docker compose down && docker compose up -d
 sleep 30
 docker restart mt5-033
+
+cd ../mt5-034
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-034
+
+cd ../mt5-035
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-035
+
+cd ../mt5-036
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-036
+
+cd ../mt5-037
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-037
+
+cd ../mt5-038
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-038
+
+cd ../mt5-039
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-039
+
+cd ../mt5-040
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-040
+
+cd ../mt5-041
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-041
+
+cd ../mt5-042
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-042
+
+cd ../mt5-043
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-043
+
+cd ../mt5-044
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-044
+
+cd ../mt5-045
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-045
+
+cd ../mt5-046
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-046
+
+cd ../mt5-047
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-047
+
+cd ../mt5-048
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-048
+
+cd ../mt5-049
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-049
+
+cd ../mt5-050
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-050
+
+cd ../mt5-051
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-051
+
+cd ../mt5-052
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-052
+
+cd ../mt5-053
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-053
+
+cd ../mt5-054
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-054
+
+cd ../mt5-055
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-055
+
+cd ../mt5-056
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-056
+
+cd ../mt5-057
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-057
+
+cd ../mt5-058
+docker compose down && docker compose up -d
+sleep 30
+docker restart mt5-058
