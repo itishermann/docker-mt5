24db36f6a2f61d8088ff121f1255eebb4722362d
Sander Ruscigno <ruscigno@gmail.com>
2023-07-02 20:08:41 -0600

    feat: update

--- build/restart.sh
+++ build/restart.sh
@@ -1,115 +1,117 @@
 docker restart mt5-001
-sleep 60
+sleep 20
 docker restart mt5-002
-sleep 60
+sleep 20
 docker restart mt5-003
-sleep 60
+sleep 20
 docker restart mt5-004
-sleep 60
+sleep 20
 docker restart mt5-005
-sleep 60
+sleep 20
 docker restart mt5-006
-sleep 60
+sleep 20
 docker restart mt5-007
-sleep 60
+sleep 20
 docker restart mt5-008
-sleep 60
+sleep 20
 docker restart mt5-009
-sleep 60
+sleep 20
 docker restart mt5-010
-sleep 60
+sleep 40
 docker restart mt5-011
-sleep 60
+sleep 20
 docker restart mt5-012
-sleep 60
+sleep 20
 docker restart mt5-013
-sleep 60
+sleep 20
 docker restart mt5-014
-sleep 60
+sleep 20
 docker restart mt5-015
-sleep 60
+sleep 20
 docker restart mt5-016
-sleep 60
+sleep 20
 docker restart mt5-017
-sleep 60
+sleep 20
 docker restart mt5-018
-sleep 60
+sleep 20
 docker restart mt5-019
-sleep 60
+sleep 20
 docker restart mt5-020
-sleep 60
+sleep 40
 docker restart mt5-021
-sleep 60
+sleep 20
 docker restart mt5-022
-sleep 60
+sleep 20
 docker restart mt5-023
-sleep 60
+sleep 20
 docker restart mt5-024
-sleep 60
+sleep 20
 docker restart mt5-025
-sleep 60
+sleep 20
 docker restart mt5-026
-sleep 60
+sleep 20
 docker restart mt5-027
-sleep 60
+sleep 20
 docker restart mt5-028
-sleep 60
+sleep 20
 docker restart mt5-029
-sleep 60
+sleep 20
 docker restart mt5-030
-sleep 60
+sleep 40
 docker restart mt5-031
-sleep 60
+sleep 20
 docker restart mt5-032
-sleep 60
+sleep 20
 docker restart mt5-033
-sleep 60
+sleep 20
 docker restart mt5-034
-sleep 60
+sleep 20
 docker restart mt5-035
-sleep 60
+sleep 20
 docker restart mt5-036
-sleep 60
+sleep 20
 docker restart mt5-037
-sleep 60
+sleep 20
 docker restart mt5-038
-sleep 60
+sleep 20
 docker restart mt5-039
-sleep 60
+sleep 20
 docker restart mt5-040
-sleep 60
+sleep 40
 docker restart mt5-041
-sleep 60
+sleep 20
 docker restart mt5-042
-sleep 60
+sleep 20
 docker restart mt5-043
-sleep 60
+sleep 20
 docker restart mt5-044
-sleep 60
+sleep 20
 docker restart mt5-045
-sleep 60
+sleep 20
 docker restart mt5-046
-sleep 60
+sleep 20
 docker restart mt5-047
-sleep 60
+sleep 20
 docker restart mt5-048
-sleep 60
+sleep 20
 docker restart mt5-049
-sleep 60
+sleep 20
 docker restart mt5-050
-sleep 60
+sleep 40
 docker restart mt5-051
-sleep 60
+sleep 20
 docker restart mt5-052
-sleep 60
+sleep 20
 docker restart mt5-053
-sleep 60
+sleep 20
 docker restart mt5-054
-sleep 60
+sleep 20
 docker restart mt5-055
-sleep 60
+sleep 20
 docker restart mt5-056
-sleep 60
+sleep 20
 docker restart mt5-057
-sleep 60
+sleep 20
 docker restart mt5-058
+sleep 20
+docker restart mt5-059
